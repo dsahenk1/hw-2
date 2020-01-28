7085b85f0c5081b77e91db8c7bf88edede6ae149
dsahenk1 <60157150+dsahenk1@users.noreply.github.com>
2020-01-27 23:27:32 -0500

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,19 @@
+import math 
+
+r=5
+
+volume= 4/3 * math.pi* math.pow(r,3)
+
+print("Volume of a sphere is: ", volume )
+
+print("Wholesale cost for 60 copies: ", (24.95*60)-(9.98*60))
+
+timeInSeconds= (6*60*60)+(52*60)+((8*60)+15)+3*((7*60)+12)+((8*60)+15)
+convertTime=(timeInSeconds/60)/60
+
+print(convertTime, "time in hours")
+
+percentageOfIncrease= (89-82)/82
+print(round(percentageOfIncrease,2)*100,'%')
+
+##https://stackoverflow.com/questions/20457038/how-to-round-to-2-decimals-with-python
